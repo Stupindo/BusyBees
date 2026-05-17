b7018323ad810a3ccbfb73eb42cf87b7bf3ae30e
stupindo <sergiy.stupnytskyy@gmail.com>
2026-05-17 09:20:01 -0400

    20260516: small fix to db chema;

--- db_schema/01_tables/06_chore_instances.sql
+++ db_schema/01_tables/06_chore_instances.sql
@@ -2,7 +2,7 @@ CREATE TABLE public.chore_instances (
     id BIGSERIAL PRIMARY KEY,
     chore_id BIGINT REFERENCES public.chores(id) ON DELETE CASCADE,
     member_id BIGINT REFERENCES public.members(id) ON DELETE CASCADE,
-    status TEXT CHECK (status IN ('pending', 'done', 'failed')),
+    status TEXT CHECK (status IN ('pending', 'done', 'failed', 'cancelled')),
     week_start_date DATE,
     notes TEXT,
     instance_date DATE DEFAULT NULL, -- NULL for weekly chores; specific date for daily chore instances
